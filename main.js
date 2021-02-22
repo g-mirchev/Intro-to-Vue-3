6a4bff0e76811cc0490acad4398d48806ac381c4
Georgi <kinggmirchev@gmail.com>
2021-02-22 18:44:05 +0200

    Leson Finished

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+            console.log(this.cart)
+        },
+        removeItem(id) {
+            const index = this.cart.indexOf(id);
+            this.cart.splice(index, 1);
+            console.log(this.cart)
+        }
+    }
 })
